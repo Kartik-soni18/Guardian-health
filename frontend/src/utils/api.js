09051515be2b04103be76463a4d61c70b49fbd6b
Kartik Soni <kartiksoni@Kartiks-MacBook-Air.local>
2026-04-02 20:43:58 +0530

    changed it to accept ap

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+// Use the direct Lambda API URL in production, but keep using the Vite proxy in development
+const API_BASE_URL = import.meta.env.PROD 
+  ? 'https://4l16rbxyai.execute-api.ap-southeast-2.amazonaws.com/default'
+  : '/api';
+
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json'
   }
